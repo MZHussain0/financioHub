31780efbd5ee466c631964af2f85987fb9f5701b
mzhussain0 <mzhussain092@gmail.com>
2023-03-25 18:36:27 +0530

    incomes and Dashboard section created

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import { useMemo, useState } from "react";
+import styled from "styled-components";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Expenses from "./components/Expenses/Expenses";
+import Income from "./components/Income/Income";
+import Navigation from "./components/Navigation/Navigation";
+import Orb from "./components/Orb/Orb";
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layouts";
+
+function App() {
+  const [active, setActive] = useState(1);
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+  return (
+    <AppStyled bg={bg}>
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
+`;
+
+export default App;
---
+++ client/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+const Expenses = () => {
+  return <div>Expenses</div>;
+};
+
+export default Expenses;
